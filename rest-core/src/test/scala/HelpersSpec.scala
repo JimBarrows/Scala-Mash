b1204b3aea6b8fd24e631f607322d0ba7846fa89
Jim Barrows <jim.barrows@bizondemand.biz>
2010-07-30 18:22:22 -0700

    Boolean parsing is tested

--- rest-core/src/test/scala/HelpersSpec.scala
+++ rest-core/src/test/scala/HelpersSpec.scala
@@ -19,5 +19,16 @@ object HelpersSpec extends Specification {
 			val date = new DateTime(2010,01,02,19,53,49,0, DateTimeZone.forID("+00:00"))
 			printWithTimeZone(date) must_== expected
 		}
+		"be able to parse boolean values" in {
+			optionalBoolean( <goof><boolean/></goof>, "boolean") must beNone			
+			optionalBoolean( <goof><boolean>0</boolean></goof>, "boolean") must     beSome[Boolean].which( _ == false)
+			optionalBoolean( <goof><boolean>1</boolean></goof>, "boolean") must     beSome[Boolean].which ( _ == true)
+			optionalBoolean( <goof><boolean>f</boolean></goof>, "boolean") must     beSome[Boolean].which ( _ == false)
+			optionalBoolean( <goof><boolean>t</boolean></goof>, "boolean") must     beSome[Boolean].which ( _ == true)
+			optionalBoolean( <goof><boolean>F</boolean></goof>, "boolean") must     beSome[Boolean].which ( _ == false)
+			optionalBoolean( <goof><boolean>T</boolean></goof>, "boolean") must     beSome[Boolean].which ( _ == true)
+			optionalBoolean( <goof><boolean>false</boolean></goof>, "boolean") must beSome[Boolean].which ( _ == false)
+			optionalBoolean( <goof><boolean>true</boolean></goof>, "boolean") must  beSome[Boolean].which ( _ == true)
+		}
 	}	
 }
