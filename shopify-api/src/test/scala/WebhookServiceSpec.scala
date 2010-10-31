8d3c0fe35a96cd8a58fd4b29ffe44562a6321e25
Jim Barrows <Jim.Barrows@gmail.com[color]>
2010-10-31 06:54:58 -0700

    adding the services spec

---
+++ shopify-api/src/test/scala/WebhookServiceSpec.scala
@@ -0,0 +1,81 @@
+package specs
+
+import org.specs.Specification
+
+import org.joda.time.DateTime
+
+import scala_mash.shopify_api.model.Webhook
+import bizondemand.utils.models.internet.{Url, Parameter}
+
+
+object WebhookServicesSpec extends Specification {
+
+	"Webhook Services" should {
+
+		shareVariables()
+		setSequential()
+
+		"create a new webhook" in{
+			val created = Webhook.create( ShopSpec.shopCredentials
+					, webhook.address
+					, webhook.topic)
+			created.address must be_==( webhook.address)
+			created.topic must be_==( webhook.topic)
+			created.createdAt must beSome[DateTime]
+			created.updatedAt must beSome[DateTime]
+			webhook = created
+		}
+		"receive a list of all webhooks" in {
+			val hookList = Webhook.listAll(ShopSpec.shopCredentials
+					, None  //limit
+					, None  //page
+					, None  //createdAtMin
+					, None  //createdAtMax
+					, None  //updateAtMin
+					, None  //updateAtMax
+					, None  //topic
+					, None  //address
+				)
+
+			hookList must contain(webhook)
+			webhookList = hookList
+		}
+
+		"receive a count of all webhooks" in {
+			val hookCount = Webhook.count(ShopSpec.shopCredentials
+					, None
+					, None)
+			hookCount must be_==( webhookList.length)
+		}
+
+		"receive a single webhook" in {
+			val hook = Webhook.get(ShopSpec.shopCredentials
+				, webhook.id.getOrElse(0))
+			hook must be_==( webhook)
+		}
+
+		"modify an existing webhook" in {
+			var changedHook = Webhook(Url("http://somewhereElse.biz/change")
+					, webhook.createdAt		//createdAt
+					, webhook.id  				//id
+					, webhook.topic				//topic
+					, webhook.updatedAt		//updateAt
+					)
+
+			val postHook = Webhook.modify(ShopSpec.shopCredentials
+					, changedHook)
+			postHook must be_==( changedHook)
+		}
+
+		"remove a webhook" in {
+		}
+
+	}
+
+	var webhook = Webhook(Url("http://bizondemand.biz")
+			, None
+			, None
+			, "orders/create"
+			, None)
+	var webhookList: List[Webhook] = Nil
+}
